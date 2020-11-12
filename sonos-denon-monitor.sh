92444768ad218b9ca63b410fd9986effe0662278
Ross <Ross Nicholson>
2020-11-12 15:22:19 +0000

    Allow variables with spaces

--- sonos-denon-monitor.sh
+++ sonos-denon-monitor.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Load DENON_AVR_NAME, DENON_INPUT and DENON_MASTER_VOLUME vars
 source $SCRIPT_DIR/variables.sh
 
-python3 $SCRIPT_DIR/scripts/sonos-denon-monitor.py $SONOS_DEVICE_NAME $DENON_INPUT $DENON_NO_AUDIO_POWER_OFF_TIMEOUT_SECS
+python3 $SCRIPT_DIR/scripts/sonos-denon-monitor.py "$SONOS_DEVICE_NAME" "$DENON_INPUT" "$DENON_NO_AUDIO_POWER_OFF_TIMEOUT_SECS"
 
 
